5f84911d069085ad2eb7525fa8b0155149d0fd5b
Aleksandr Belousov <alexandr.belousov@lykke.com>
2020-03-29 22:09:03 +0400

    https://swisschain.atlassian.net/browse/SIR-222

--- src/Brokerage.Common/Persistence/IAccountRequisitesRepository.cs
+++ src/Brokerage.Common/Persistence/IAccountRequisitesRepository.cs
@@ -1,10 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Brokerage.Common.Domain.AccountRequisites;
 
 namespace Brokerage.Common.Persistence
 {
     public interface IAccountRequisitesRepository
     {
+        Task<IReadOnlyCollection<AccountRequisites>> SearchAsync(
+            long accountId,
+            int limit,
+            long? cursor,
+            bool sortAsc);
         Task<AccountRequisites> GetAsync(string brokerAccountRequisitesId);
 
         Task<AccountRequisites> AddOrGetAsync(AccountRequisites brokerAccount);
