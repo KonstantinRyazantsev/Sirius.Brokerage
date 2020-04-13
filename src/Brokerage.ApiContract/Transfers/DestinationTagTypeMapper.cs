f44725ab75237ec0983ae6065e2c231e02d17993
Aleksandr Belousov <alexandr.belousov@lykke.com>
2020-04-13 20:03:12 +0400

    finished withdrawals proto

---
+++ src/Brokerage.ApiContract/Transfers/DestinationTagTypeMapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Swisschain.Sirius.Brokerage.ApiContract.Transfers
+{
+    public static class DestinationTagTypeMapper
+    {
+        public static Sdk.Primitives.DestinationTagType ToDomain(DestinationTagType value)
+        {
+            return value switch
+            {
+                DestinationTagType.Text => Sdk.Primitives.DestinationTagType.Text,
+                DestinationTagType.Number => Sdk.Primitives.DestinationTagType.Number,
+                _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+            };
+        }
+
+        public static Sdk.Primitives.DestinationTagType? ToDomain(NullableDestinationTagType value)
+        {
+            return value == null ? 
+                default(Sdk.Primitives.DestinationTagType?) : 
+                ToDomain(value.Value);
+        }
+
+        public static DestinationTagType FromDomain(Sdk.Primitives.DestinationTagType value)
+        {
+            return value switch
+            {
+                Sdk.Primitives.DestinationTagType.Text => DestinationTagType.Text,
+                Sdk.Primitives.DestinationTagType.Number => DestinationTagType.Number,
+                _ => throw new ArgumentOutOfRangeException(nameof(value), value, null)
+            };
+        }
+
+        public static NullableDestinationTagType FromDomain(Sdk.Primitives.DestinationTagType? value)
+        {
+            return value == null
+                ? new NullableDestinationTagType {Null = NullValue.NullValue}
+                : new NullableDestinationTagType {Value = FromDomain(value.Value)};
+        }
+    }
+}
