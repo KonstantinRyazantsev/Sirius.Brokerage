bceaf8f0ff4fc0f1c308f9cbbd4d2798eb4006fb
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-07-15 17:15:02 +0400

    Added address formatter

--- src/Brokerage.Common/ReadModels/Blockchains/Blockchain.cs
+++ src/Brokerage.Common/ReadModels/Blockchains/Blockchain.cs
@@ -1,8 +1,14 @@
-﻿namespace Brokerage.Common.ReadModels.Blockchains
+﻿using System;
+using Swisschain.Sirius.Sdk.Primitives;
+
+namespace Brokerage.Common.ReadModels.Blockchains
 {
     public sealed class Blockchain
     {
         public string Id { get; set; }
         public Protocol Protocol { get; set; }
+        public NetworkType NetworkType { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
     }
 }
