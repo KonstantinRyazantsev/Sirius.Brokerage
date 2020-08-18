dacbb35265880a6a2c5ceccdee87992aad3a5874
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-08-18 08:40:31 +0400

    Added migration with sequence columns

---
+++ src/Brokerage.Common/Migrations/20200818044002_UnifiedMessageProcessing.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Brokerage.Common.Migrations
+{
+    public partial class UnifiedMessageProcessing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Sequence",
+                schema: "brokerage",
+                table: "broker_accounts",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<long>(
+                name: "Sequence",
+                schema: "brokerage",
+                table: "accounts",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                schema: "brokerage",
+                table: "broker_accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                schema: "brokerage",
+                table: "accounts");
+        }
+    }
+}
